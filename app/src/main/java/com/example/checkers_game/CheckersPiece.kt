81fea65c4d702b62871046ffdb3d05d4276b2a9f
TomYDoma <qwerty135261@gmail.com>
2023-01-19 23:43:16 +0300

    (52)

--- app/src/main/java/com/example/checkers_game/CheckersPiece.kt
+++ app/src/main/java/com/example/checkers_game/CheckersPiece.kt
@@ -1,5 +1,5 @@
 package com.example.checkers_game
 
-data class CheckersPiece(val col: Int, val row: Int, val player: Player, val rank: Chessman, val resID: Int) {
+data class CheckersPiece(val col: Int, val row: Int, val player: Player, val chessman: Chessman, val resID: Int) {
 
 }
--- app/src/main/java/com/example/checkers_game/ChessGame.kt
+++ app/src/main/java/com/example/checkers_game/ChessGame.kt
@@ -1,10 +1,23 @@
 package com.example.checkers_game
 
 object ChessGame {
-    var piecesBox = mutableSetOf<CheckersPiece>()
+    private var piecesBox = mutableSetOf<CheckersPiece>()
 
     init {
         reset()
+    }
+
+    fun clear(){
+        piecesBox.removeAll(piecesBox)
+    }
+
+    fun addPiece(piece: CheckersPiece){
+        piecesBox.add(piece)
+
+    }
+
+    fun canKnight(from: Square, to: Square): Boolean{
+        return false
     }
 
     fun movePiece(from: Square, to: Square){
@@ -22,21 +35,22 @@ object ChessGame {
             piecesBox.remove(it)
         }
         piecesBox.remove(movingPiece)
-        piecesBox.add(movingPiece.copy(col = toCol, row = toRow))
+        addPiece(movingPiece.copy(col = toCol, row = toRow))
 
     }
 
     fun reset() {
+        clear()
         piecesBox.removeAll(piecesBox)
         for (i in 0..7 step 2){
-            piecesBox.add(CheckersPiece(0+i,0, Player.WHITE, Chessman.ORDINARY, R.drawable.white_ordinary))
-            piecesBox.add(CheckersPiece(1+i,1, Player.WHITE, Chessman.ORDINARY, R.drawable.white_ordinary))
-            piecesBox.add(CheckersPiece(0+i,2, Player.WHITE, Chessman.ORDINARY, R.drawable.white_ordinary))
+            addPiece(CheckersPiece(0+i,0, Player.WHITE, Chessman.ORDINARY, R.drawable.white_ordinary))
+            addPiece(CheckersPiece(1+i,1, Player.WHITE, Chessman.ORDINARY, R.drawable.white_ordinary))
+            addPiece(CheckersPiece(0+i,2, Player.WHITE, Chessman.ORDINARY, R.drawable.white_ordinary))
         }
         for (i in 0..7 step 2){
-            piecesBox.add(CheckersPiece(1+i,7, Player.BLACK, Chessman.ORDINARY, R.drawable.black_ordinary))
-            piecesBox.add(CheckersPiece(0+i,6, Player.BLACK, Chessman.ORDINARY, R.drawable.black_ordinary))
-            piecesBox.add(CheckersPiece(1+i,5, Player.BLACK, Chessman.ORDINARY, R.drawable.black_ordinary))
+            addPiece(CheckersPiece(1+i,7, Player.BLACK, Chessman.ORDINARY, R.drawable.black_ordinary))
+            addPiece(CheckersPiece(0+i,6, Player.BLACK, Chessman.ORDINARY, R.drawable.black_ordinary))
+            addPiece(CheckersPiece(1+i,5, Player.BLACK, Chessman.ORDINARY, R.drawable.black_ordinary))
         }
 
 
@@ -74,7 +88,7 @@ object ChessGame {
             desc += " "
             desc += pieceAt(col, row)?.let {
                 val white = it.player == Player.WHITE
-                when (it.rank) {
+                when (it.chessman) {
                     Chessman.ORDINARY -> if (white) "б" else "ч"
                     Chessman.KING -> if (white) "д" else "Д"
                 }
